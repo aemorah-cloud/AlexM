8c235dae9575e5232496aaf0932ef6346acd6d08
Alex Mora <aleemora@espol.edu.ec>
2026-06-23 22:57:06 -0400

    Initial Setup, plus weather api connection

---
+++ lambda/weather_test.py
@@ -0,0 +1,23 @@
+import requests
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_KEY = os.getenv("WEATHER_API_KEY")
+
+CITY = "Toronto"
+
+url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    data = response.json()
+    
+    print("🌦️ Weather Data Retrieved Successfully")
+    print(json.dumps(data, indent=2))
+else:
+    print("❌ Error:", response.status_code)
+    print(response.text)
