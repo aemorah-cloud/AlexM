ac78ed1590e84cbffa11e983775e1528c45a63ec
Alex M <aemorah@gmail.com>
2026-06-25 21:45:55 -0400

    CHange structure of lambda_weather.py to include a new function for processing weather data.

--- real-time-weather-analytics-aws/lambda/lambda_weather.py
+++ real-time-weather-analytics-aws/lambda/lambda_weather.py
@@ -1,31 +1,71 @@
 import json
 import boto3
-import requests
-from datetime import datetime
+import urllib.request
 import os
+from datetime import datetime
 
 s3 = boto3.client("s3")
 
-def lambda_handler(event, context):
 
+def fetch_weather():
     API_KEY = os.environ["WEATHER_API_KEY"]
-    BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
     CITY = "Toronto"
 
     url = f"http://api.openweathermap.org/data/2.5/weather?q={CITY}&appid={API_KEY}&units=metric"
-    response = requests.get(url)
-    data = response.json()
 
-    timestamp = datetime.utcnow().strftime("%Y-%m-%d_%H-%M-%S")
-    file_name = f"weather_{CITY}_{timestamp}.json"
+    with urllib.request.urlopen(url) as response:
+        data = json.loads(response.read().decode())
+
+    return data
+
+def transform_weather(data):
+    return {
+        "timestamp": datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%SZ"),
+        "city": data["name"],
+        "country": data["sys"]["country"],
+
+        "temperature": data["main"]["temp"],
+        "feels_like": data["main"]["feels_like"],
+        "temp_min": data["main"]["temp_min"],
+        "temp_max": data["main"]["temp_max"],
+
+        "humidity": data["main"]["humidity"],
+        "pressure": data["main"]["pressure"],
+        "visibility": data.get("visibility", None),
+
+        "wind_speed": data["wind"]["speed"],
+        "wind_direction": data["wind"].get("deg", None),
+        "wind_gust": data["wind"].get("gust", None),
+
+        "cloudiness": data["clouds"]["all"],
+
+        "weather": data["weather"][0]["main"],
+        "description": data["weather"][0]["description"],
+
+        "sunrise": data["sys"]["sunrise"],
+        "sunset": data["sys"]["sunset"]
+    }
+
+
+def upload_to_s3(clean_data):
+    BUCKET_NAME = os.environ["S3_BUCKET_NAME"]
+
+    timestamp = clean_data["timestamp"].replace(":", "-")
+    file_name = f"weather_Toronto_{timestamp}.json"
 
     s3.put_object(
         Bucket=BUCKET_NAME,
         Key=f"raw/{file_name}",
-        Body=json.dumps(data)
+        Body=json.dumps(clean_data)
     )
+
+
+def lambda_handler(event, context):
+    raw_data = fetch_weather()
+    clean_data = transform_weather(raw_data)
+    upload_to_s3(clean_data)
 
     return {
         "statusCode": 200,
-        "body": json.dumps("Data uploaded successfully")
+        "body": json.dumps("Weather data uploaded successfully")
     }
